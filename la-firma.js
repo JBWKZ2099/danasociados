ff79fc347153ddd5a3383501f426018009b818a6
Ivan Ramirez <iramirez@fabricadesoluciones.com>
2020-03-25 22:27:07 -0600

    ajustes publicaciones

--- la-firma.js
+++ la-firma.js
@@ -1,15 +1,15 @@
-$(window).on("load", function(){
-    if( $(window).width()>990 ) {
-        wheight = $(window).height()-$(".navbar").outerHeight();
-        $(".bg-la-firma").height( wheight+"px" );
-    }
-});
+// $(window).on("load", function(){
+//     if( $(window).width()>990 ) {
+//         wheight = $(window).height()-$(".navbar").outerHeight();
+//         $(".bg-la-firma").height( wheight+"px" );
+//     }
+// });
 
-$(window).resize(function(){
-    if( $(window).width()<768 ) {
-        $(".bg-la-firma").removeAttr("style");
-    } else {
-        wheight = $(window).height()-$(".navbar").outerHeight();
-        $(".bg-la-firma").height( wheight+"px" );
-    }
-});
+// $(window).resize(function(){
+//     if( $(window).width()<768 ) {
+//         $(".bg-la-firma").removeAttr("style");
+//     } else {
+//         wheight = $(window).height()-$(".navbar").outerHeight();
+//         $(".bg-la-firma").height( wheight+"px" );
+//     }
+// });
